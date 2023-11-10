0e80272bb4938959162e6be6f27f5d71c5278f0c
Logan Ferreira <logan.ferreira@etu.unilim.fr>
2023-11-10 08:39:42 +0100

    Improve MainMenu and PickableObject

--- src/UnityProject/Assets/Scripts/MainMenu.cs
+++ src/UnityProject/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,15 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
+    }
+
 }
