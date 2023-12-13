9202dd86cf6f1d36212afe50fa85711e909a00c2
Logan Ferreira <logan.ferreira@etu.unilim.fr>
2023-12-13 10:48:24 +0100

    [Update] PickableObject.cs

--- src/UnityProject/Assets/Scripts/PickableObject.cs
+++ src/UnityProject/Assets/Scripts/PickableObject.cs
@@ -1,79 +1,77 @@
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    public class PickableObject : MonoBehaviour
+public class PickableObject : MonoBehaviour
+{
+    public Transform player;
+    public Transform playerCam;
+    public float throwForce = 10;
+
+    private bool hasPlayer = false;
+    private bool beingCarried = false;
+    private bool touched = false;
+
+    private static GameObject carriedObject = null; // Objet actuellement porté
+
+    void Update()
     {
-        public Transform player;
-        public Transform playerCam;
-        public float throwForce = 10;
+        float dist = Vector3.Distance(gameObject.transform.position, player.position);
 
-        private bool hasPlayer = false;
-        private bool beingCarried = false;
-        private bool touched = false;
-
-        private Vector3 objectPos;
-        private float distance;
-
-        // Start is called before the first frame update
-        void Start()
+        if (dist < 2f)
         {
-            objectPos = transform.position;
+            hasPlayer = true;
+        }
+        else
+        {
+            hasPlayer = false;
         }
 
-        // Update is called once per frame
-        void Update()
+        if (hasPlayer && Input.GetKeyDown(KeyCode.E) && !beingCarried && carriedObject == null) // Vérifier si aucun objet n'est déjà porté
         {
-            float dist = Vector3.Distance(gameObject.transform.position, player.position);
-
-            if (dist < 3f)
-            {
-                hasPlayer = true;
-            }
-            else
-            {
-                hasPlayer = false;
-            }
-
-            if (hasPlayer && Input.GetKeyDown(KeyCode.E))
-            {
-                GetComponent<Rigidbody>().isKinematic = true;
-                distance = Vector3.Distance(gameObject.transform.position, player.position);
-                beingCarried = true;
-            }
-
-            if (beingCarried)
-            {
-                if (touched)
-                {
-                    GetComponent<Rigidbody>().isKinematic = false;
-                    beingCarried = false;
-                    touched = false;
-                }
-
-                if (Input.GetMouseButtonDown(0))
-                {
-                    GetComponent<Rigidbody>().isKinematic = false;
-                    beingCarried = false;
-                    GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
-                }
-                else if (Input.GetMouseButtonDown(1))
-                {
-                    GetComponent<Rigidbody>().isKinematic = false;
-                    beingCarried = false;
-                }
-
-                // Update the position of the object relative to the player camera
-                objectPos = playerCam.position + playerCam.forward * distance;
-                transform.position = objectPos;
-            }
+            GetComponent<Rigidbody>().isKinematic = true;
+            beingCarried = true;
+            carriedObject = gameObject; // Mettre à jour l'objet actuellement porté
         }
 
-        private void OnTriggerEnter(Collider other)
+        if (beingCarried && carriedObject == gameObject) // Vérifier si l'objet actuellement porté est celui-ci
         {
-            if (beingCarried)
+            if (touched)
             {
-                touched = true;
+                GetComponent<Rigidbody>().isKinematic = false;
+                transform.parent = null;
+                beingCarried = false;
+                touched = false;
+                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce * -50);
+                Debug.Log(playerCam.forward * throwForce);
+                carriedObject = null; // Réinitialiser l'objet actuellement porté
             }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                beingCarried = false;
+                carriedObject = null; // Réinitialiser l'objet actuellement porté
+                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent<Rigidbody>().isKinematic = false;
+                beingCarried = false;
+                carriedObject = null; // Réinitialiser l'objet actuellement porté
+            }
+
+            Vector3 objectPos = playerCam.position + playerCam.forward * 3f; // Ajuste la distance entre l'objet et la caméra du joueur
+            transform.position = objectPos;
         }
     }
+
+    void OnTriggerEnter()
+    {
+        if (beingCarried)
+        {
+            touched = true;
+        }
+    }
+}
