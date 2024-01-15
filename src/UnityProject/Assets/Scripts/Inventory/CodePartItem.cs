5a9a6a7096a0cf96e504bdedfff30325c218fbc8
Willem Boender <willem.boender@etu.unilim.fr>
2024-01-15 12:30:06 +0100

    [Add] Sheet code

---
+++ src/UnityProject/Assets/Scripts/Inventory/CodePartItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace inventoryGestion
+{
+    [CreateAssetMenu(menuName = "Items/Sheet code Item Data")]
+    public class CodePartItem : ItemData
+    {
+        [SerializeField] public string codePartName;
+    }
+    
+}
