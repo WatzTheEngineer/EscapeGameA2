603a5734da01e1379c2a96eb4f2d04e2e160ff83
Willem Boender <willem.boender@etu.unilim.fr>
2024-01-15 16:00:45 +0100

    [Add] Diskette animation

---
+++ src/UnityProject/Assets/Scripts/Controllers/InsertDisquetteTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class InsertDisquetteTrigger: MonoBehaviour, ITriggerable
+    {
+        private bool isInserted = false;
+        private Inventory inventory;
+        [SerializeField] private Item ActionItem;
+        public void Start()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void Trigger()
+        {
+            isInserted = !isInserted;
+            gameObject.SetActive(true);
+            gameObject.GetComponent<Animator>().Play("OPEN",0,0.0f);
+            inventory = FindObjectOfType<Inventory>();
+            inventory.PickItem(ActionItem);
+        }
+    
+        public bool GetHeadPlugState()
+        {
+            return isInserted;
+        }   
+    }
+}
