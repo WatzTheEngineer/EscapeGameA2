e17fead18ccaf382b2fe77a3845cd83d201e8ff0
Watz <watztheengineer@gmail.com>
2024-01-16 10:06:05 +0100

    [Refactor] tutorial

--- src/UnityProject/Assets/Scripts/Controllers/TutoController.cs
+++ src/UnityProject/Assets/Scripts/Controllers/TutoController.cs
@@ -1,92 +1,99 @@
 ﻿using System.Collections;
 using UnityEngine;
-using UnityEngine.Serialization;
 
-namespace DefaultNamespace
+namespace Controllers
 {
-    public class TutoController : MonoBehaviour
+    public class TutorialController : MonoBehaviour
     {
-        [SerializeField] private GameObject panelZqsdCaps;
-        [SerializeField] private GameObject panelSpaceCap;
-        [SerializeField] private GameObject panelECap;
-        [SerializeField] private GameObject panelClickCap;
-        [SerializeField] private GameObject panelShiftCap;
-        [SerializeField] private CanvasGroup canvasGroup;
-        [SerializeField] private float waitTimeBetweenFade;
+        [SerializeField] public GameObject zqsdPanel;
+        [SerializeField] public GameObject spacePanel;
+        [SerializeField] public GameObject ePanel;
+        [SerializeField] public GameObject clickPanel;
+        [SerializeField] public GameObject shiftPanel;
+        [SerializeField] public GameObject tabPanel;
+        [SerializeField] public CanvasGroup canvasGroup;
+        [SerializeField] public float waitTimeBetweenFade;
+
+        private bool _isClickActive;
+        private bool _isEActive;
+        private bool _isShiftActive;
+        private bool _isSpaceActive;
+        private bool _isTabActive;
 
         private bool _isZqsdActive = true;
-        private bool _isSpaceActive;
-        private bool _isEActive;
-        private bool _isClickActive;
-        private bool _isShiftActive;
 
         private void Start()
         {
-            ActivatePanel(panelZqsdCaps, true);
-            ActivatePanel(panelSpaceCap, false);
-            ActivatePanel(panelECap, false);
-            ActivatePanel(panelClickCap, false);
-            ActivatePanel(panelShiftCap, false);
+            ActivatePanel(zqsdPanel, true);
+            ActivatePanel(spacePanel, false);
+            ActivatePanel(ePanel, false);
+            ActivatePanel(clickPanel, false);
+            ActivatePanel(shiftPanel, false);
+            ActivatePanel(tabPanel, false);
         }
 
         private void Update()
         {
-            if (canvasGroup.alpha >= 1)
-            {
-                CheckForKeyPress();
-            }
+            if (canvasGroup.alpha >= 1) CheckForKeyPress();
         }
 
         private void CheckForKeyPress()
         {
             if (_isZqsdActive && IsZqsdKey())
             {
-                StartCoroutine(TransitionToNextPanel(panelZqsdCaps, panelSpaceCap, waitTimeBetweenFade));
+                StartCoroutine(TransitionToNextPanel(zqsdPanel, spacePanel, waitTimeBetweenFade));
                 _isZqsdActive = false;
                 _isSpaceActive = true;
             }
             else if (_isSpaceActive && Input.GetKeyDown(KeyCode.Space))
             {
-                StartCoroutine(TransitionToNextPanel(panelSpaceCap, panelECap, waitTimeBetweenFade));
+                StartCoroutine(TransitionToNextPanel(spacePanel, ePanel, waitTimeBetweenFade));
                 _isSpaceActive = false;
                 _isEActive = true;
             }
             else if (_isEActive && Input.GetKeyDown(KeyCode.E))
             {
-                StartCoroutine(TransitionToNextPanel(panelECap,panelClickCap,waitTimeBetweenFade));
+                StartCoroutine(TransitionToNextPanel(ePanel, clickPanel, waitTimeBetweenFade));
                 _isEActive = false;
                 _isClickActive = true;
             }
             else if (_isClickActive && (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)))
             {
-                StartCoroutine(TransitionToNextPanel(panelClickCap,panelShiftCap,waitTimeBetweenFade));
+                StartCoroutine(TransitionToNextPanel(clickPanel, shiftPanel, waitTimeBetweenFade));
                 _isClickActive = false;
                 _isShiftActive = true;
             }
-            else if (_isShiftActive && (Input.GetKeyDown(KeyCode.LeftShift)))
+            else if (_isShiftActive && Input.GetKeyDown(KeyCode.LeftShift))
             {
-                StartCoroutine(FadeOutPanel(panelShiftCap));
+                StartCoroutine(TransitionToNextPanel(shiftPanel, tabPanel, waitTimeBetweenFade));
                 _isShiftActive = false;
+                _isTabActive = true;
+            }
+            else if (_isTabActive && Input.GetKeyDown(KeyCode.Tab))
+            {
+                StartCoroutine(FadeOutPanel(tabPanel));
+                _isTabActive = false;
             }
         }
 
-        private IEnumerator TransitionToNextPanel(GameObject currentPanel, GameObject nextPanel, float delay)
+        public IEnumerator TransitionToNextPanel(GameObject currentPanel, GameObject nextPanel, float delay)
         {
             yield return StartCoroutine(FadeOutPanel(currentPanel));
             yield return StartCoroutine(FadeInPanel(nextPanel, delay));
         }
 
-        private IEnumerator FadeOutPanel(GameObject panel)
+        public IEnumerator FadeOutPanel(GameObject panel)
         {
             while (canvasGroup.alpha > 0)
             {
                 canvasGroup.alpha -= Time.deltaTime;
                 yield return null;
             }
+
             ActivatePanel(panel, false);
         }
 
-        private IEnumerator FadeInPanel(GameObject panel, float delay)
+        public IEnumerator FadeInPanel(GameObject panel, float delay)
         {
             yield return new WaitForSeconds(delay);
             ActivatePanel(panel, true);
@@ -98,7 +105,7 @@ namespace DefaultNamespace
             }
         }
 
-        private bool IsZqsdKey()
+        public bool IsZqsdKey()
         {
             return Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) ||
                    Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D);
