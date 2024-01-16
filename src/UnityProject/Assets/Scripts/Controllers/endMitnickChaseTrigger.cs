b2bdabeedda59ca4e5ba44f2b8958a33127e77bd
Willem Boender <willem.boender@etu.unilim.fr>
2024-01-16 17:36:34 +0100

    [Add] end of the scene
    Pb without doors 0:35

--- src/UnityProject/Assets/Scripts/Controllers/endMitnickChaseTrigger.cs
+++ src/UnityProject/Assets/Scripts/Controllers/endMitnickChaseTrigger.cs
@@ -5,18 +5,38 @@ using UnityEngine.UI;
 
 public class endMitnickChaseTrigger : MonoBehaviour, ITriggerable
 {
+    public bool isVisible = false;
+
     [SerializeField] GameObject endUi;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.SetActive(false);
     }
-    
+
     public void Trigger()
     {
+        isVisible = !isVisible;
         Debug.Log("Trigger");
         endUi.SetActive(true);
-        
+        Cursor.visible = isVisible;
+        Cursor.lockState = isVisible ? CursorLockMode.None : CursorLockMode.Locked;
+        togglePause();
     }
-    
+
+    public bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+
+    }
 }
