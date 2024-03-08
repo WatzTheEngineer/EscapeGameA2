8748c734979d6477ef17472fd0f57966bbb94254
Logan Ferreira <logan.ferreira@etu.unilim.fr>
2024-03-08 18:42:07 +0100

    [Fix] DoorController script

--- src/ProjectSOLID/Assets/Scripts/Controllers/doorController.cs
+++ src/ProjectSOLID/Assets/Scripts/Controllers/doorController.cs
@@ -1,15 +1,45 @@
+using System;
 using UnityEngine;
 
 public class doorController : MonoBehaviour
 {
-    public float vitesseDescendre = 0.01f; 
+    public bool opening = false;
+    public Transform door;
+
 
     void Update()
     {
+        if (opening)
+        {
+  
+            OpenDoor();
+        }
         
-        float deplacementY = vitesseDescendre * Time.deltaTime;
+        
 
         
-        transform.Translate(Vector3.down * deplacementY);
+        
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if(obj.transform.name == "Player"){opening = true;}
+
+        if (obj.transform.name == "DoorCloseTrigger"){opening = false;}
+    }
+
+    void OpenDoor()
+    {
+        float movement = 0.7f * Time.deltaTime;
+        Debug.Log(door.position.y);
+        Debug.Log(door.position.y > 0);
+        if (door.position.y - 5.5 > 0 ) {
+            door.position = new Vector3(door.position.x, door.position.y - movement, door.position.z);
+        }
+    
+            
+        
+        
+
     }
 }
