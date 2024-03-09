21e1317f6b1bb42eeb271fe323c52df60cd9fa85
Logan Ferreira <logan.ferreira@etu.unilim.fr>
2024-03-09 23:11:40 +0100

    [Update] S_KataBombe [Fix] doorController.cs

--- src/ProjectSOLID/Assets/Scripts/Controllers/doorController.cs
+++ src/ProjectSOLID/Assets/Scripts/Controllers/doorController.cs
@@ -5,6 +5,7 @@ public class doorController : MonoBehaviour
 {
     public bool opening = false;
     public Transform door;
+    public GameObject trigger;
 
 
     void Update()
@@ -34,6 +35,10 @@ public class doorController : MonoBehaviour
 
         if (door.position.y - 5.5 > 0 ) {
             door.position = new Vector3(door.position.x, door.position.y - movement, door.position.z);
+        }
+        else if (door.position.y - 5.5 <= 0)
+        {
+            trigger.gameObject.SetActive(false);
         }
     
             
